afdbd1b0ffc2fedba2d88decd0530ad2f4b5fec1
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-16 19:06:04 +1100

    heroku cors fix

--- backend/app.js
+++ backend/app.js
@@ -19,13 +19,8 @@ app.use("/uploads", express.static("uploads"));
 // define routes
 app.use("/pdf", pdfRouter);
 
-<<<<<<< HEAD
-app.listen(5001, function () {
-    console.log("CORS-enabled web server listening on port", 5001);
-=======
 app.listen(PORT, function () {
     console.log("CORS-enabled web server listening on port", PORT);
->>>>>>> f68a67c6224aaf06e1bab36136fee3efdd9d5edc
 });
 
 module.exports = app;
