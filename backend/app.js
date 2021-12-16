15e1e4dc9e45cc2973b5569a2fb170e868439faf
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-16 17:27:08 +1100

    deployed server to heroku

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const pdfRouter = require("./routes/pdfRoutes");
 
+const PORT = process.env.PORT || 5001;
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,8 +18,8 @@ app.use("/uploads", express.static("uploads"));
 // define routes
 app.use("/pdf", pdfRouter);
 
-app.listen(5001, function () {
-    console.log("CORS-enabled web server listening on port", 5001);
+app.listen(PORT, function () {
+    console.log("CORS-enabled web server listening on port", PORT);
 });
 
 module.exports = app;
