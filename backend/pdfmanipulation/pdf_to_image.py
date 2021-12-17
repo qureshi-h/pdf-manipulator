034fb1727921a151ce5e3a1f01bf78b2a583b14e
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-17 13:43:14 +1100

    added pil import statement

--- backend/pdfmanipulation/pdf_to_image.py
+++ backend/pdfmanipulation/pdf_to_image.py
@@ -1,22 +1,25 @@
 
 import subprocess
 import sys
+from PIL import Image
 
 # def install(package):
-subprocess.check_call([sys.executable, "-m", "pip", "install", "pdf2image"])
+# subprocess.check_call([sys.executable, "-m", "pip", "install", "pdf2image"])
 
-# print(install("pdf2image"))
+# # print(install("pdf2image"))
 
-# import pkg_resources
-# installed_packages = pkg_resources.working_set
-# installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
-# print(installed_packages_list)
+# # import pkg_resources
+# # installed_packages = pkg_resources.working_set
+# # installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
+# # print(installed_packages_list)
 
-from pdf2image import convert_from_path
-# import sys
+# from pdf2image import convert_from_path
+# # import sys
 
-images = convert_from_path(sys.argv[1])
-for i in range(len(images)):
-    path = sys.argv[1].split(".pdf")[0] + str(i) + '.jpg'
-    print(path)
-    images[i].save(path, 'JPEG')
+# images = convert_from_path(sys.argv[1])
+# for i in range(len(images)):
+#     path = sys.argv[1].split(".pdf")[0] + str(i) + '.jpg'
+#     print(path)
+#     images[i].save(path, 'JPEG')
+
+print("hello world")
