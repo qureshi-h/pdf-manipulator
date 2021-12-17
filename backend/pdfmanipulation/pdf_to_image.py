78272ab93d28873a80509e46ed8606e1612c1a68
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-17 13:26:30 +1100

    removed pdf2image lib from python file

--- backend/pdfmanipulation/pdf_to_image.py
+++ backend/pdfmanipulation/pdf_to_image.py
@@ -12,11 +12,13 @@
 # installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
 # print(installed_packages_list)
 
-from pdf2image import convert_from_path
-import sys
+# from pdf2image import convert_from_path
+# # import sys
 
-images = convert_from_path(sys.argv[1])
-for i in range(len(images)):
-    path = sys.argv[1].split(".pdf")[0] + str(i) + '.jpg'
-    print(path)
-    images[i].save(path, 'JPEG')
+# images = convert_from_path(sys.argv[1])
+# for i in range(len(images)):
+#     path = sys.argv[1].split(".pdf")[0] + str(i) + '.jpg'
+#     print(path)
+#     images[i].save(path, 'JPEG')
+
+print("hello world")
