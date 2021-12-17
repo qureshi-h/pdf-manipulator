6477e1350e18952b57fa4857dbb86b6ae704fb93
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-17 15:31:53 +1100

    removed print statements

--- backend/pdfmanipulation/pdf_to_image.py
+++ backend/pdfmanipulation/pdf_to_image.py
@@ -12,11 +12,7 @@ import os
 # # installed_packages = pkg_resources.working_set
 # # installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in installed_packages])
 # # print(installed_packages_list)
-print(os.listdir("./uploads/1/alpha"))
-
 from pdf2image import convert_from_path
-
-print(sys.argv[1])
 
 images = convert_from_path(sys.argv[1])
 for i in range(len(images)):
