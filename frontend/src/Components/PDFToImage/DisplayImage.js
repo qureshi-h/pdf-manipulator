212ad464627a2e8e7049b19333ab615ebe68ea85
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-20 12:51:49 +1100

    removed absolute url from api

--- frontend/src/Components/PDFToImage/DisplayImage.js
+++ frontend/src/Components/PDFToImage/DisplayImage.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export const DisplayImage = ({ image, index, handleChange }) => {
+    console.log(image);
     return (
         <div>
-            <img src={image.image} alt="pdf" className="pdfToImage" />
+            <img
+                src={
+                    "https://server-online-pdf-manager.herokuapp.com/" +
+                    image.image
+                }
+                alt="pdf"
+                className="pdfToImage"
+            />
 
             <div className="checkBoxContainer">
                 <input
