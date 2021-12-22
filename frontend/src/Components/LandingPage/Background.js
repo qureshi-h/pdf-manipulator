760b96a5fde5f6cec5bfb80a78a7e59c83c5fb79
Hamza <hamza.ahmedqureshi@hotmail.com>
2021-12-22 18:33:28 +1100

    minor design improvements

--- frontend/src/Components/LandingPage/Background.js
+++ frontend/src/Components/LandingPage/Background.js
@@ -26,7 +26,16 @@ export const Background = () => {
                     type="button"
                     className="btn btn-primary btn-lg titleButton"
                 >
-                    <h4 style={{ padding: "0vh 1vw 0vh 1vw" }}>Get Started</h4>
+                    <h4
+                        style={{
+                            padding: "0vh 1vw 0vh 1vw",
+                            height: "100%",
+                            alignItems: "center",
+                            display: "flex",
+                        }}
+                    >
+                        Get Started
+                    </h4>
                 </button>
             </Link>
         </div>
