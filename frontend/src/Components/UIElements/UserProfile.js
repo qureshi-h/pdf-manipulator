4fa707af7bb0dec843e0e54558aba35e99f0aed8
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-01-16 22:55:23 +1100

    Added Logout label

--- frontend/src/Components/UIElements/UserProfile.js
+++ frontend/src/Components/UIElements/UserProfile.js
@@ -1,34 +1,37 @@
 import React from "react";
+import Tooltip from "@mui/material/Tooltip";
 
 import NoPic from "../../res/NoPic.jpeg";
 
 export const UserProfile = () => {
     return (
-        <div
-            style={{
-                alignItems: "center",
-                justifyContent: "right",
-                marginRight: "4vw",
-                display: "flex",
-            }}
-            onClick={() => {
-                localStorage.setItem("loggedIn", false);
-                alert("You have been logged out");
-                window.location.reload();
-            }}
-        >
-            <h4 className="userName">
-                HELLO, &nbsp; {localStorage.getItem("name")}
-            </h4>
-            <img
-                src={
-                    localStorage.getItem("picture") === "none"
-                        ? NoPic
-                        : localStorage.getItem("picture")
-                }
-                className="userImage"
-                alt="user"
-            />
-        </div>
+        <Tooltip title="Logout">
+            <div
+                style={{
+                    alignItems: "center",
+                    justifyContent: "right",
+                    marginRight: "4vw",
+                    display: "flex",
+                }}
+                onClick={() => {
+                    localStorage.setItem("loggedIn", false);
+                    alert("You have been logged out");
+                    window.location.reload();
+                }}
+            >
+                <h4 className="userName">
+                    HELLO, &nbsp; {localStorage.getItem("name").split(" ")[0]}
+                </h4>
+                <img
+                    src={
+                        localStorage.getItem("picture") === "none"
+                            ? NoPic
+                            : localStorage.getItem("picture")
+                    }
+                    className="userImage"
+                    alt="user"
+                />
+            </div>
+        </Tooltip>
     );
 };
