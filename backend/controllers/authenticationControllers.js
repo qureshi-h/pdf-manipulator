56d5899b79b9253d3a6336ae15dd30db24bae165
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-01-16 23:07:24 +1100

    Added password encryption

--- backend/controllers/authenticationControllers.js
+++ backend/controllers/authenticationControllers.js
@@ -1,14 +1,23 @@
 const Register = require("../models/register");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 exports.addUser = async (req, res) => {
+    let { name, email, password, picture } = req.body;
     try {
-        let result = await Register.add(Object.values(req.body));
-
-        if (result.rowCount === 1) {
-            res.status(200).json({ status_code: 200 });
-        } else {
-            throw Error("User Not Added");
-        }
+        bcrypt.genSalt(saltRounds, (error, salt) => {
+            if (error) throw error;
+            bcrypt.hash(password, salt, async (error, hash) => {
+                if (error) throw error;
+                const result = await Register.add([name, email, hash, picture]);
+                if (result.rowCount === 1) {
+                    res.status(200).json({ status_code: 200 });
+                } else {
+                    throw Error("User Not Added");
+                }
+            });
+        });
     } catch (error) {
         res.status(400).json({
             status_code: 400,
