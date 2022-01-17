def116be31d4f26ce6e36a14b75c0d9ce7bd7fab
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-01-17 16:20:35 +1100

    linked privacy policy page

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -6,6 +6,7 @@ import { Background } from "../Components/LandingPage/Background";
 import { Tools } from "../Components/LandingPage/Tools";
 import { NavigationBar } from "../Components/UIElements/NavBar/NavigationBar";
 import { Helmet } from "react-helmet";
+import { Footer } from "../Components/UIElements/Footer";
 
 export const LandingPage = () => {
     const [loading, setLoading] = React.useState(false);
@@ -24,10 +25,13 @@ export const LandingPage = () => {
                     <Loader />
                 </div>
             )}
-            <div style={{ display: loading ? "none" : "block" }}>
+            <div
+                style={{ display: loading ? "none" : "block", height: "100vh" }}
+            >
                 <Background setLoading={setLoading} />
                 <Tools />
                 <NavigationBar />
+                <Footer />
             </div>
         </div>
     );
